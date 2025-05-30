d12f31c782b95e8c2814330902ea911a2c17ddda
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-30 11:42:35 -0300

    feat: footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>Developed by Bárbara Azevedo.</h2>
+        </footer>
+    )
+}
+
+export default Footer;
