568a2b73bf90091f5aac8c6e3b08d090b6adabb0
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-31 14:33:08 -0300

    feat: database cards implementation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,8 @@ import Footer from '../../components/Footer';
 import Banner from '../../components/Banner';
 import Title from '../../components/Title';
 import Card from '../../components/Card';
+import videos from '../../json/db.json';
+import styles from './Home.module.css';
 
 function Home() {
     return (
@@ -12,7 +14,11 @@ function Home() {
             <Title>
                 <h1>A place to store your videos and movies!</h1>
             </Title>
-                <Card id='1' titulo='bonifácio cat' cover='https://thecatapi.com/api/images/get?format-src&type=png' />
+                <section className={styles.container}>
+                    {videos.map((video) => {
+                        return <Card {...video} key={video.id} />
+                    })}
+                </ section>    
             <Footer />
         </>
     );
