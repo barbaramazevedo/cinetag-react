7b19c4af270b054e87fa61d4a7f131d181a77645
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-31 15:51:06 -0300

    feat: add conditional to favorite icon

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,22 @@
+import { useFavoriteContext } from '../../contexts/favorites';
 import styles from './Card.module.css';
 import favoriteIcon from './favorite.png';
+import unfavoriteIcon from './unfavorite.png';
 
 function Card({ id, title, cover }) {
+    const {favorite, addFavorite} = useFavoriteContext();
+    const isFavorite = favorite.some((fav) => fav.id === id);
+    const icon = !isFavorite ? favoriteIcon : unfavoriteIcon
+
     return (
         <div className={styles.container}>
             <img src={cover} alt={title} className={styles.cover} />
             <h2>{title}</h2>
-            <img src={favoriteIcon}
+            <img src={icon}
                 alt="favorite movie"
-                className={styles.favorite} />
+                className={styles.favorite} 
+                onClick={() => { addFavorite({id, title, cover})}}
+            />
         </div>
     )
 }
