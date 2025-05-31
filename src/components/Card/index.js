5968bec0afc6f80f703b40e0a457ce4cdf3c0dcf
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-31 13:56:21 -0300

    feat: card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import styles from './Card.module.css';
+import favoriteIcon from './favorite.png';
+
+function Card({ id, title, cover }) {
+    return (
+        <div className={styles.container}>
+            <img src={cover} alt={title} className={styles.cover} />
+            <h2>{title}</h2>
+            <img src={favoriteIcon}
+                alt="favorite movie"
+                className={styles.favorite} />
+        </div>
+    )
+}
+
+export default Card;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Banner from '../../components/Banner';
 import Title from '../../components/Title';
+import Card from '../../components/Card';
 
 function Home() {
     return (
@@ -11,6 +12,7 @@ function Home() {
             <Title>
                 <h1>A place to store your videos and movies!</h1>
             </Title>
+                <Card id='1' titulo='bonifácio cat' cover='https://thecatapi.com/api/images/get?format-src&type=png' />
             <Footer />
         </>
     );
