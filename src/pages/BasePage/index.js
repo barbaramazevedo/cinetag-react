80bd1542e9eee06bdb41cc154ed4f675bda6215e
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-31 16:52:07 -0300

    feat: import files check

--- src/pages/BasePage/index.js
+++ src/pages/BasePage/index.js
@@ -1,7 +1,7 @@
-import Header from "components/Header";
-import Container from "components/Container";
-import Footer from "components/Footer";
-import FavoritesProvider from "./contexts/favorites";
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import FavoritesProvider from "../../contexts/favorites";
 import { Outlet } from "react-router-dom";
 
 function BasePage() {
